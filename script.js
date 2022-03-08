a1545b69141d88b3f868292261d14be27e46dace
Thapelo Mmakola <mthapelothapza@gmail.com>
2022-03-08 13:41:55 +0200

    form works fine and can submit

---
+++ script.js
@@ -0,0 +1,17 @@
+function sendMessage() {
+    alert('form works')
+    // Email.send({
+    //     Host : "smtp.gmail.com",
+    //     Username : "201501759@student.uj.ac.za",
+    //     Password : "mthapelothapza",
+    //     To : 'mthapelothapza@gmail.com',
+    //     From : document.getElementById('email'),
+    //     Subject : "Demo",
+    //     Body : `Name: ${document.getElementById('name').value} 
+    //     <br/>   Email: ${document.getElementById('email').value}
+    //     <br/>   Phone no: ${document.getElementById('phone').value}
+    //     <br/>   Message:  ${document.getElementById('message').value}`
+    // }).then(
+    //   message => alert(message)
+    // );
+}
